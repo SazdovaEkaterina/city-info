a70ff4d05a6a6127a34df368593bd71e074ac3d4
Ekaterina Sazdova <74919977+SazdovaEkaterina@users.noreply.github.com>
2023-07-19 15:37:14 +0200

    remove accidentally generated table for a DTO

---
+++ CityInfo/CityInfo.API/Migrations/20230719132849_RemoveAccidentallyGeneratedDtoTable.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CityInfo.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveAccidentallyGeneratedDtoTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PointOfInterestDto");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PointOfInterestDto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CityId = table.Column<int>(type: "INTEGER", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PointOfInterestDto", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PointOfInterestDto_Cities_CityId",
+                        column: x => x.CityId,
+                        principalTable: "Cities",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PointOfInterestDto_CityId",
+                table: "PointOfInterestDto",
+                column: "CityId");
+        }
+    }
+}
